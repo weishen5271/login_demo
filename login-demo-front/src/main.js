f4737567879d02403f6e6f9f2eaef348dd7a3d89
沈 伟 <shenwei@freedotech.com>
2020-09-18 14:43:34 +0800

    添加登录页面

--- login-demo-front/src/main.js
+++ login-demo-front/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+
 import App from './App.vue'
+// import './plugins/element.js'
+import router from './router'
+
+import ElementUI from 'element-ui' //element-ui的全部组件
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router, //使用路由配置
 }).$mount('#app')
